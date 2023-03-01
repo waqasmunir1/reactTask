2f0cde0ae7c512d01b2929050281f7c109e68551
Waqas Munir~~ <waqasmunir@isoft-tech.pk>
2023-03-01 11:50:09 +0500

    task: Completed

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,118 +1,113 @@
 "use client"; // this is a client component
-
-import { tableData } from '@/utils/data';
-import React, { useEffect, useState } from 'react';
-
+import { tableData } from "@/utils/data";
+import React, { useState } from "react";
 
 const Table = () => {
-    const [searchQuery, setSearchQuery] = useState('');
-    const [sortOrder, setSortOrder] = useState('asc');
-    const [currentPage, setCurrentPage] = useState(1);
-    const [search, setSearch] = useState(tableData);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState(tableData);
 
-    const handleSearchInputChange = event => {
-        setSearchQuery(event.target.value);
-        setCurrentPage(1);
-    };
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+    setCurrentPage(1);
+  }; 
 
-    const filteredData = search.filter(
-        item =>
-            item.title.toLowerCase()?.includes(searchQuery.toLowerCase()) ||
-            item.year.toLowerCase().includes(searchQuery.toLowerCase())
-    );
+  const filteredData = search.filter(
+    (item) =>
+      item.id.toLowerCase()?.includes(searchQuery.toLowerCase()) ||
+      item.title.toLowerCase()?.includes(searchQuery.toLowerCase()) ||
+      item.year.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
-    const handleSortOrderChange = event => {
-        setSortOrder(event.target.value);
-    };
+  const handleSortOrderChange = (event) => {
+    setSortOrder(event.target.value);
+  };
 
-    const sortedData = filteredData.sort((a, b) => {
-        if (sortOrder === 'asc') {
-            return a.year - b.year;
-        } else {
-            return b.year - a.year;
-        }
-    });
+  const sortedData = filteredData.sort((a, b) => {
+    if (sortOrder === "asc") {
+      return a.year - b.year;
+    } else {
+      return b.year - a.year;
+    }
+  });
 
-    const PAGE_SIZE = 10;
+  const PAGE_SIZE = 10;
 
+  const totalPages = Math.ceil(filteredData.length / PAGE_SIZE);
 
-    const totalPages = Math.ceil(filteredData.length / PAGE_SIZE);
+  const handlePreviousClick = () => {
+    setCurrentPage((currentPage) => currentPage - 1);
+  };
 
-    const handlePreviousClick = () => {
-        setCurrentPage(currentPage => currentPage - 1);
-    };
+  const handleNextClick = () => {
+    setCurrentPage((currentPage) => currentPage + 1);
+  };
 
-    const handleNextClick = () => {
-        setCurrentPage(currentPage => currentPage + 1);
-    };
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+  const paginatedData = sortedData.slice(startIndex, endIndex);
 
-    const startIndex = (currentPage - 1) * PAGE_SIZE;
-    const endIndex = startIndex + PAGE_SIZE;
-    const paginatedData = sortedData.slice(startIndex, endIndex);
+  return (
+    <React.Fragment>
+      <div className="filter-wrapper">
+        <div className="search-field">
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={handleSearchInputChange}
+            placeholder="Search..."
+            className="search-input"
+          />
+        </div>
+        <div style={{ display: "flex", alignItems: "flex-start" }}>
+          <label htmlFor="sort-by">Sort by year:</label>
+          <select
+            id="sort-by"
+            value={sortOrder}
+            onChange={handleSortOrderChange}
+          >
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </div>
+      </div>
 
-    return (
-        <React.Fragment>
-            <div className='filter-wrapper'>
-                <div className="search-field">
-                    <input
-                        type="text"
-                        value={searchQuery}
-                        onChange={handleSearchInputChange}
-                        placeholder="Search..."
-                        className="search-input"
-                    />
-                </div>
-                <div style={{ display: "flex", alignItems: "flex-start" }}>
-                    <label htmlFor="sort-by">Sort by year:</label>
-                    <select id="sort-by" value={sortOrder} onChange={handleSortOrderChange}>
-                        <option value="asc">Ascending</option>
-                        <option value="desc">Descending</option>
-                    </select>
-                </div>
-            </div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Year</th>
+            <th>Title</th>
+            <th>description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {!paginatedData.length ? (
+            <h1>No Data found...</h1>
+          ) : (
+            paginatedData?.map((item, index) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.year}</td>
+                <td>{item.title}</td>
+                <td>{item.description}</td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
 
-
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Year</th>
-                        <th>Title</th>
-                        <th>Desc</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {!paginatedData.length ? (
-                        <h1>No Data found...</h1>
-                    ) : (
-                        paginatedData?.map((item, index) => (
-                            <tr key={item.id}>
-                                <td>{item.id}</td>
-                                <td>{item.year}</td>
-                                <td>{item.title}</td>
-                                <td>{item.description}</td>
-                            </tr>
-                        ))
-                    )}
-                </tbody>
-            </table>
-
-            <div className="pagination">
-                <button
-                    onClick={handlePreviousClick}
-                    disabled={currentPage === 1}
-                >
-                    Previous
-                </button>
-                <button
-                    onClick={handleNextClick}
-                    disabled={currentPage === totalPages}
-                >
-                    Next
-                </button>
-            </div>
-        </React.Fragment>
-    );
+      <div className="pagination">
+        <button onClick={handlePreviousClick} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <button onClick={handleNextClick} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Table;
