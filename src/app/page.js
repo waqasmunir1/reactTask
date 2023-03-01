fea44787d777f493cc8f3677b5541e6ba965e9c8
Waqas Munir~~ <waqasmunir@isoft-tech.pk>
2023-03-01 13:29:47 +0500

    formatted completed task

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,14 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-import Table from '@/components/Table'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+import Table from "@/components/Table";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  
   return (
     <main className={styles.main}>
-      <Table/>
+      <Table />
     </main>
-  )
+  );
 }
